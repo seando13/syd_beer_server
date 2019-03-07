d1beb2c20b31b7ce7aadfa46f3c4228c9cf3c8e4
Sean McCusker <sean.mccusker@gmail.com>
2019-03-07 15:47:01 +1100

    added controllers to breweries route

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,10 @@
-class BreweryController < ApplicationController
+class BreweriesController < ApplicationController
   def search
     @breweries = Brewery.where(:location => params[:location])
+  end
+
+  def show
+    brewery = Brewery.find_by :name => params[:id]
+    render :json => brewery, :include => :beers
   end
 end
